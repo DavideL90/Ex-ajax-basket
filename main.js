4d7bc6e9d3594a228363764725ea3666025c3a37
DavideL90 <davide.lecci@icloud.com>
2018-04-17 17:22:11 +0200

    completed

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
          url:'https://www.boolean.careers/api/array/basket?n=' + numberRequested,
          method:'GET',
          success: function(data){
-            console.log(data.response);
             playerArray = data.response;
+            console.log(playerArray);
             fillTheList(playerArray);
          },
          error: function(){
@@ -22,8 +22,14 @@ $(document).ready(function(){
       });
    });
    //When click on a playerCode shows statistics
-   $(document).on('click', 'playerCode', function(){
-
+   $(document).on('click', '.playerCode', function(){
+      //take the code of the selected player
+      var actualCode = $(this).children('.code').text();
+      //declare an obj with all the player's stats
+      var actualPlayer = giveStats(playerArray, actualCode);
+      $('#stats').children().remove();
+      //print on screen all the stats
+      printStats(actualPlayer);
    });
 });
 
@@ -31,6 +37,37 @@ $(document).ready(function(){
 function fillTheList(arrPlayer){
    var list = $('#playerList');
    for (var i = 0; i < arrPlayer.length; i++) {
-      list.append('<div class="playerCode"> Codice giocatore: ' + arrPlayer[i].playerCode + '</div>');
+      list.append('<div class="playerCode"> Codice giocatore: ' + '<span class="code">' + arrPlayer[i].playerCode + '</span></div>');
    }
+}
+//function that gives the stas of the player
+function giveStats(arrPlayer, currentCode){
+   //variable to see if I found the playercode
+   var isFound = false;
+   //variable to search every element of the array
+   var cont = 0;
+   //var to append the result
+   do{
+      if(arrPlayer[cont].playerCode == currentCode){
+         var player = arrPlayer[cont];
+         isFound = true;
+      }
+      else{
+         cont++;
+      }
+   }while((!isFound) && (cont <= arrPlayer.length));
+
+   return player;
+}
+//function that prints on screen the stats
+function printStats(currentPlayer){
+   var stats = $('#stats');
+   var twoPoints = currentPlayer.twoPoints;
+   var threePoints = currentPlayer.threePoints;
+   var totPoints = currentPlayer.points;
+   stats.append('<div> Punti: ' + currentPlayer.points + '</div>' +
+                '<div> % tiri da 2 Punti: ' + (twoPoints * 100 / totPoints).toFixed(2) + '</div>' +
+                '<div> % tiri da 3 Punti: ' + (threePoints * 100 / totPoints).toFixed(2) + '</div>' +
+                '<div> Rimbalzi: ' + currentPlayer.rebounds + '</div>' +
+                '<div> Falli: ' + currentPlayer.fouls + '</div>');
 }

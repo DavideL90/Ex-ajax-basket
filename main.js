c75370eb27f57e59cd2f0e1c39cb2b7f169f53fa
DavideL90 <davide.lecci@icloud.com>
2018-04-17 16:03:09 +0200

    need only to show stats

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+//declaring an empty array to store the players
+var playerArray = [];
 $(document).ready(function(){
-   //Al click sul bottone ottengo un array
-   //con i giocatori
+   //When click on button generate an array
+   //containing the number of players given
+   //as input
+   $('#insertNumb').click(function(){
+      var numberRequested = $('#playerNumb').val();
+      $('#playerNumb').val('');
+      //make an ajax request to obtain the array
+      $.ajax({
+         url:'https://www.boolean.careers/api/array/basket?n=' + numberRequested,
+         method:'GET',
+         success: function(data){
+            console.log(data.response);
+            playerArray = data.response;
+            fillTheList(playerArray);
+         },
+         error: function(){
+            alert('Error');
+         }
+      });
+   });
+   //When click on a playerCode shows statistics
+   $(document).on('click', 'playerCode', function(){
+
+   });
 });
+
+//function to fill the player list
+function fillTheList(arrPlayer){
+   var list = $('#playerList');
+   for (var i = 0; i < arrPlayer.length; i++) {
+      list.append('<div class="playerCode"> Codice giocatore: ' + arrPlayer[i].playerCode + '</div>');
+   }
+}
